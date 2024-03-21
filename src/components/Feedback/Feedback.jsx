e5cd9c6e38239ca82a735024e157cbfbb9354ca9
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-03-21 20:55:18 +0200

    3 step

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,13 +1,20 @@
 import App from "../../App";
 
 const Feedback = ({ feedbackTypes }) => {
-  return (
-    <div>
-      <p>Good: {feedbackTypes.good}</p>
-      <p>Neutral: {feedbackTypes.neutral}</p>
-      <p>Bad: {feedbackTypes.bad}</p>
-    </div>
-  );
+  const totalFeedback =
+    feedbackTypes.good + feedbackTypes.neutral + feedbackTypes.bad;
+
+  if (totalFeedback === 0) {
+    return "No feedback yet";
+  } else {
+    return (
+      <div>
+        <p>Good: {feedbackTypes.good}</p>
+        <p>Neutral: {feedbackTypes.neutral}</p>
+        <p>Bad: {feedbackTypes.bad}</p>
+      </div>
+    );
+  }
 };
 
 export default Feedback;
